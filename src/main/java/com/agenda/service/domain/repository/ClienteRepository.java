beb7d80b1b769ac173889b5028201f4ee47f130e
Dianine <dianine23@gmail.com>
2023-11-04 23:03:05 -0300

    feat: add Interface ClienteRepository

---
+++ src/main/java/com/agenda/service/domain/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.agenda.service.domain.repository;
+
+import com.agenda.service.domain.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByCpf(String cpf);
+    //Optional<Cliente> findByEmail(String email);
+}
