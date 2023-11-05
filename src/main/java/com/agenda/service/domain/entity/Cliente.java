b0c2fb19a99b3a9f9ea9843961e9b2b78d4cabb3
Dianine <dianine23@gmail.com>
2023-11-05 13:50:57 -0300

    chore: add @NotBlank model Cliente

--- src/main/java/com/agenda/service/domain/entity/Cliente.java
+++ src/main/java/com/agenda/service/domain/entity/Cliente.java
@@ -1,6 +1,7 @@
 package com.agenda.service.domain.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -15,9 +16,12 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Nome do Clinete é obrigatório")
     private String nome;
+    @NotBlank(message = "Sobrenome do Clinete é obrigatório")
     private String sobrenome;
     private String email;
+    @NotBlank(message = "CPF do Clinete é obrigatório")
     private String cpf;
 
 }
